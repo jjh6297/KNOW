c280b951f630bdd4ba2db26013b9ff64ad9fa422
jjh6297 <35516615+jjh6297@users.noreply.github.com>
2026-03-03 16:57:15 +0900

    Add files via upload

--- KNOWN.py
+++ KNOWN.py
@@ -1,30 +1,23 @@
 from __future__ import print_function
 import tensorflow.keras
-# from tensorflow.keras.datasets import mnist, fashion_mnist, cifar10
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.utils import np_utils
 from tensorflow.keras.optimizers import  Adam
 import tensorflow as tf
 import cv2
 import numpy as np
 from tensorflow.keras.models import Model
-# from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler, History
 from tensorflow.keras import backend as K
 from itertools import combinations
 from scipy.spatial import distance
 import sys
 from tensorflow.keras.constraints import unit_norm
-# from itertools import combinations
 import h5py
 import random
 from tensorflow.keras.layers import Input, Dense, Permute, Reshape
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, AveragePooling2D, GlobalMaxPooling2D, LeakyReLU, Reshape, Concatenate
 from tensorflow.keras.layers import Activation, Dropout, Flatten, concatenate, Maximum, Lambda, Multiply, Add, add, multiply, SpatialDropout2D, GaussianDropout, AlphaDropout, GlobalAveragePooling2D
-# from tensorflow.keras.utils.training_utils import multi_gpu_model
 from tensorflow.keras import regularizers
 from tensorflow.keras import initializers
-# import scipy.io as sio
-# import math
+
 
 
 def trAct_1D_Exp(x,compressed, numExp):
@@ -97,107 +90,3 @@ def get_FCLayer_pred(layer, predictor,NumLength=5):
 def get_BiasLayer_pred(layer, predictor,NumLength=5):
     return layer[:,NumLength-1] + 1.*predictor.predict([layer, layer[:,1:NumLength] - layer[:,0:NumLength-1]], batch_size = 100000)[:,0]
 
-     
-    
-def get_BiasLayer_pred(layer, predictor, NumLength=5):
-    Layer = layer
-    dim = Layer.shape[1]
-    minval = np.tile(Layer[:,0:1]  ,(1,dim)) 
-    Layer=Layer -minval   
-    maxval = np.tile(np.max(Layer,1, keepdims=True)-np.min(Layer,1, keepdims=True)+1e-8  ,(1, dim)) 
-
-    Layer=Layer/(maxval+0.00001)/2.       
-    return (layer[:,NumLength-1] +  predictor.predict([Layer, Layer[:,1:NumLength] - Layer[:,0:NumLength-1]], batch_size = 100000)[:,0])*(maxval[:,0]+0.00001)*2. +minval[:,0]
-
-
-
-global WeightsSet
-global BiasSet
-WeightsSet=[]
-BiasSet=[]
-class WeightForecasting(tensorflow.keras.callbacks.Callback):
-    def __init__(self):
-        super(WeightForecasting, self).__init__()
-        self.model2 = KNOWN(5)
-        self.model2.compile(loss='mae', optimizer=Adam(),metrics=['mae'])
-        self.model2.load_weights('NKNOWN_Conv_13.h5')
-        
-        self.model3 = KNOWN(5)
-        self.model3.compile(loss='mae', optimizer=Adam(),metrics=['mae'])
-        self.model3.load_weights('NKNOWN_FC_13.h5')						
-
-        self.model4 = KNOWN(5)
-        self.model4.compile(loss='mae', optimizer=Adam(),metrics=['mae'])       
-        self.model4.load_weights('NKNOWN_Bias_13.h5')
-        self.cnt=0
-
-        
-
-    def on_epoch_end(self, epoch, logs=None):
-        global WeightsSet, BiasSet        
-        if epoch==0:
-            self.cnt = 0
-            for ll in self.model.layers:
-                WW = ll.get_weights()
-                if len(WW)>0 and len(WW)<4:
-                    if len(WW)==1:
-                        WeightsSet.append(np.zeros((WW[0].shape+(5,))))
-                        BiasSet.append([])
-                        
-                    else: 	
-                        WeightsSet.append(np.zeros((WW[0].shape+(5,))))
-                        BiasSet.append(np.zeros((WW[1].shape+(5,))))
-                    self.cnt=self.cnt+1	
-            print('Init')
-        idx = epoch%5
-                  
-        
-        self.cnt = 0
-        for ll in self.model.layers:
-            WW = ll.get_weights()
-
-            if len(WW)>0 and len(WW)<4:
-                if len(WW)==1:
-                    if len(WW[0].shape)==4:
-                        WeightsSet[self.cnt][:,:,:,:,idx] = WW[0]
-                    elif len(WW[0].shape)==3:
-                        WeightsSet[self.cnt][:,:,:,idx] = WW[0]         
-                    else:
-                        WeightsSet[self.cnt][:,:,idx] = WW[0]                                
-
-                
-                else:
-                    if len(WW[0].shape)==4:
-                        WeightsSet[self.cnt][:,:,:,:,idx] = WW[0]
-                    elif len(WW[0].shape)==3:
-                        WeightsSet[self.cnt][:,:,:,idx] = WW[0]         
-                    else:
-                        WeightsSet[self.cnt][:,:,idx] = WW[0]        
-                    BiasSet[self.cnt][:,idx] = WW[1]
-
-                self.cnt=self.cnt+1	
-        if epoch%5==4 and epoch>0:
-            self.cnt = 0
-            for ll in self.model.layers:
-                WW = ll.get_weights()
-                if len(WW)>0 and len(WW)<4:
-                    if len(WW)==1:
-                        if len(WW[0].shape)==4:
-                            NewWeights = np.array(get_ConvLayer_pred(WeightsSet[self.cnt],self.model2,5))
-                        if len(WW[0].shape)==3:
-                            NewWeights = np.array(get_DepthwiseConvLayer_pred(WeightsSet[self.cnt],self.model2,5))
-                        if len(WW[0].shape)==2:
-                            NewWeights = np.array(get_FCLayer_pred(WeightsSet[self.cnt],self.model3,5))
-                        ll.set_weights([NewWeights])						                    
-                    else: 	
-                        if len(WW[0].shape)==4:
-                            NewWeights = np.array(get_ConvLayer_pred(WeightsSet[self.cnt],self.model2,5))
-                        if len(WW[0].shape)==3:
-                            NewWeights = np.array(get_DepthwiseConvLayer_pred(WeightsSet[self.cnt],self.model2,5))
-                        if len(WW[0].shape)==2:
-                            NewWeights = np.array(get_FCLayer_pred(WeightsSet[self.cnt],self.model3,5))
-                        NewBias = np.array(get_BiasLayer_pred(BiasSet[self.cnt],self.model4,5))
-                        ll.set_weights([NewWeights, NewBias])		                  
-                    self.cnt=self.cnt+1
-					
-            print('Forecasting Done')
